a9b83fc564e8e97bf6b36087e31acb67e07b011b
Quantum10101 <quantum@quantumjester.com>
2024-07-17 18:44:01 -0500

    Illustrate a couple ways to use helper classes

---
+++ ops/HelperOne.ts
@@ -0,0 +1,3 @@
+export class HelperOne {
+	str: string = "helper one class";
+}
--- ops/JustAnAction.ts
+++ ops/JustAnAction.ts
@@ -1,4 +1,5 @@
 import { MenuAction } from "../lib/MenuAction.ts";
+import { HelperOne } from "./HelperOne.ts";
 
 export class JustAnAction extends MenuAction {
 	title: string = "Just an Action";
@@ -10,9 +11,19 @@ export class JustAnAction extends MenuAction {
 		
 		console.log(this.menuSystem.data["stuff"]);
 		console.log("i do action now");
+		
+		const helperOne = new HelperOne();
+		console.log(helperOne.str);
+		const helperTwo = new HelperTwo();
+		console.log(helperTwo.str);
+		
 		console.log(data);
 		
 		//this.menuSystem.nextAction(MenuActionEnum.MainMenu);
 		this.menuSystem.lastAction();
 	}
+}
+
+class HelperTwo {
+	str: string = "helper two class";
 }
