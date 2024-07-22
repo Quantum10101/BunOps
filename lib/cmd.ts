41cecb62d3774f410657e56ad386fad9dfb07adf
Quantum10101 <quantum@quantumjester.com>
2024-07-22 18:02:10 -0500

    Added cmd with easy access shortcuts for ANSI color and text codes

---
+++ lib/cmd.ts
@@ -0,0 +1,65 @@
+export class cmd {
+	static CLS = "\x1b[2J\x1b[0;0H";
+	
+	static color = {
+		RESET: "\x1B[0m",
+		REVERSE: "\x1B[7m",
+		RED: "\x1B[91m",
+		RED_BG: "\x1B[101m",
+		RED_DARK: "\x1B[31m",
+		RED_DARK_BG: "\x1B[41m",
+		GREEN: "\x1B[92m",
+		GREEN_BG: "\x1B[102m",
+		GREEN_DARK: "\x1B[32m",
+		GREEN_DARK_BG: "\x1B[42m",
+		BLUE: "\x1B[94m",
+		BLUE_BG: "\x1B[104m",
+		BLUE_DARK: "\x1B[34m",
+		BLUE_DARK_BG: "\x1B[44m",
+		YELLOW: "\x1B[93m",
+		YELLOW_BG: "\x1B[103m",
+		YELLOW_DARK: "\x1B[33m",
+		YELLOW_DARK_BG: "\x1B[43m",
+		MAGENTA: "\x1B[95m",
+		MAGENTA_BG: "\x1B[105m",
+		MAGENTA_DARK: "\x1B[35m",
+		MAGENTA_DARK_BG: "\x1B[45m",
+		CYAN: "\x1B[96m",
+		CYAN_BG: "\x1B[106m",
+		CYAN_DARK: "\x1B[36m",
+		CYAN_DARK_BG: "\x1B[46m",
+		WHITE: "\x1B[97m",
+		WHITE_BG: "\x1B[107m",
+		WHITE_DARK: "\x1B[37m",
+		WHITE_DARK_BG: "\x1B[47m",
+		BLACK: "\x1B[90m",
+		BLACK_BG: "\x1B[100m",
+		BLACK_DARK: "\x1B[30m",
+		BLACK_DARK_BG: "\x1B[40m",
+	};
+	
+	static text = {
+		BOLD: "\x1B[1m",
+		END_BOLD: "\x1B[22m",
+		UNDERLINE: "\x1B[4m",
+		END_UNDERLINE: "\x1B[24m",
+		ITALICS: "\x1B[3m",
+		END_ITALICS: "\x1B[23m",
+		STRIKETHROUGH: "\x1B[9m",
+		END_STRIKETHROUGH: "\x1B[29m",
+	};
+	
+	static move = {
+		UP: (n: number = 1) => `\x1B[${n}A`,
+		DOWN: (n: number = 1) => `\x1B[${n}B`,
+		RIGHT: (n: number = 1) => `\x1B[${n}C`,
+		LEFT: (n: number = 1) => `\x1B[${n}D`
+	};
+	
+	static cursor = {
+		SAVE: "\x1B[s",
+		RESTORE: "\x1B[u",
+		HIDE: "\x1B[?25l",
+		SHOW: "\x1B[?25h"
+	};
+}
--- ops/MenuItem.ts
+++ ops/MenuItem.ts
@@ -1,5 +1,6 @@
 import { MenuActionEnum } from "./MenuActionEnum.ts";
 import { MenuAction } from "../lib/MenuAction.ts";
+import { cmd } from "../lib/cmd.ts";
 
 export class MenuItem extends MenuAction {
 	title: string = "Menu Item";
@@ -9,7 +10,63 @@ export class MenuItem extends MenuAction {
 	action(): void {
 		this.menuSystem.menuHistory.push(this.menuActionEnum);
 		
+		process.stdout.write(`${cmd.CLS}`);
+		
 		process.stdout.write(`${this.title}\n\n`);
+		
+		process.stdout.write(`Color tests:\n`);
+		
+		process.stdout.write(`${cmd.color.RED}RED${cmd.color.RESET} `);
+		process.stdout.write(`${cmd.color.RED_BG}RED_BG${cmd.color.RESET} `);
+		process.stdout.write(`${cmd.color.RED_DARK}RED_DARK${cmd.color.RESET} `);
+		process.stdout.write(`${cmd.color.RED_DARK_BG}RED_DARK_BG${cmd.color.RESET}\n`);
+		
+		process.stdout.write(`${cmd.color.GREEN}GREEN${cmd.color.RESET} `);
+		process.stdout.write(`${cmd.color.GREEN_BG}GREEN_BG${cmd.color.RESET} `);
+		process.stdout.write(`${cmd.color.GREEN_DARK}GREEN_DARK${cmd.color.RESET} `);
+		process.stdout.write(`${cmd.color.GREEN_DARK_BG}GREEN_DARK_BG${cmd.color.RESET}\n`);
+		
+		process.stdout.write(`${cmd.color.BLUE}BLUE${cmd.color.RESET} `);
+		process.stdout.write(`${cmd.color.BLUE_BG}BLUE_BG${cmd.color.RESET} `);
+		process.stdout.write(`${cmd.color.BLUE_DARK}BLUE_DARK${cmd.color.RESET} `);
+		process.stdout.write(`${cmd.color.BLUE_DARK_BG}BLUE_DARK_BG${cmd.color.RESET}\n`);
+		
+		process.stdout.write(`${cmd.color.YELLOW}YELLOW${cmd.color.RESET} `);
+		process.stdout.write(`${cmd.color.YELLOW_BG}YELLOW_BG${cmd.color.RESET} `);
+		process.stdout.write(`${cmd.color.YELLOW_DARK}YELLOW_DARK${cmd.color.RESET} `);
+		process.stdout.write(`${cmd.color.YELLOW_DARK_BG}YELLOW_DARK_BG${cmd.color.RESET}\n`);
+		
+		process.stdout.write(`${cmd.color.MAGENTA}MAGENTA${cmd.color.RESET} `);
+		process.stdout.write(`${cmd.color.MAGENTA_BG}MAGENTA_BG${cmd.color.RESET} `);
+		process.stdout.write(`${cmd.color.MAGENTA_DARK}MAGENTA_DARK${cmd.color.RESET} `);
+		process.stdout.write(`${cmd.color.MAGENTA_DARK_BG}MAGENTA_DARK_BG${cmd.color.RESET}\n`);
+		
+		process.stdout.write(`${cmd.color.CYAN}CYAN${cmd.color.RESET} `);
+		process.stdout.write(`${cmd.color.CYAN_BG}CYAN_BG${cmd.color.RESET} `);
+		process.stdout.write(`${cmd.color.CYAN_DARK}CYAN_DARK${cmd.color.RESET} `);
+		process.stdout.write(`${cmd.color.CYAN_DARK_BG}CYAN_DARK_BG${cmd.color.RESET}\n`);
+		
+		process.stdout.write(`${cmd.color.WHITE}WHITE${cmd.color.RESET} `);
+		process.stdout.write(`${cmd.color.WHITE_BG}WHITE_BG${cmd.color.RESET} `);
+		process.stdout.write(`${cmd.color.WHITE_DARK}WHITE_DARK${cmd.color.RESET} `);
+		process.stdout.write(`${cmd.color.WHITE_DARK_BG}WHITE_DARK_BG${cmd.color.RESET}\n`);
+		
+		process.stdout.write(`${cmd.color.BLACK}BLACK${cmd.color.RESET} `);
+		process.stdout.write(`${cmd.color.BLACK_BG}BLACK_BG${cmd.color.RESET} `);
+		process.stdout.write(`${cmd.color.BLACK_DARK}BLACK_DARK${cmd.color.RESET} `);
+		process.stdout.write(`${cmd.color.BLACK_DARK_BG}BLACK_DARK_BG${cmd.color.RESET}\n`);
+		
+		process.stdout.write(`NORMAL${cmd.color.RESET} `);
+		process.stdout.write(`${cmd.text.BOLD}BOLD${cmd.color.RESET} `);
+		process.stdout.write(`${cmd.text.BOLD}END_${cmd.text.END_BOLD}BOLD${cmd.color.RESET} `);
+		process.stdout.write(`${cmd.text.UNDERLINE}UNDERLINE${cmd.color.RESET} `);
+		process.stdout.write(`${cmd.text.UNDERLINE}END_${cmd.text.END_UNDERLINE}UNDERLINE${cmd.color.RESET} `);
+		process.stdout.write(`${cmd.text.ITALICS}ITALICS${cmd.color.RESET} `);
+		process.stdout.write(`${cmd.text.ITALICS}END_${cmd.text.END_ITALICS}ITALICS${cmd.color.RESET} `);
+		process.stdout.write(`${cmd.text.STRIKETHROUGH}STRIKETHROUGH${cmd.color.RESET} `);
+		process.stdout.write(`${cmd.text.STRIKETHROUGH}END_${cmd.text.END_STRIKETHROUGH}STRIKETHROUGH${cmd.color.RESET} `);
+		
+		process.stdout.write(`\n\n`);
 		
 		this.menu([
 			{menuAction: MenuActionEnum.MainMenu, command: "mm"},
